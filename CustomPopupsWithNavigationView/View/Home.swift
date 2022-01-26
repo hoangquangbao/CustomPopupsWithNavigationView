5178589530e237150410a827ddb45314489d082a
Quang Bao <hoangquangbao.developer@gmail.com>
2022-01-26 11:47:13 +0700

    Show popupNavigationView

--- CustomPopupsWithNavigationView/View/Home.swift
+++ CustomPopupsWithNavigationView/View/Home.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct Home: View {
+    
+    @State var showPopup: Bool = false
+    
     var body: some View {
         
         NavigationView{
             
             Button("Show Popup"){
-                
+                showPopup.toggle()
             }
             .navigationTitle("Custom Popup's")
+        }
+        .popupNavigationView(horizontalPadding: 40, show: $showPopup) {
+            
+            // MARK: Your Popup content which will also performs navigations
+            List{
+                ForEach(tasks){task in
+                    NavigationLink(task.taskTitle){
+                        Text(task.taskDescription)
+                            .navigationTitle("Destination")
+                    }
+                }
+            }
+            .navigationTitle("Popup Navigation")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(false)
         }
     }
 }
