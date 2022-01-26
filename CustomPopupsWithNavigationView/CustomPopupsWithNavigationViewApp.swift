28a4fdf92bdb074098fb418ec8ddd54663926570
Quang Bao <hoangquangbao.developer@gmail.com>
2022-01-26 09:54:01 +0700

    Initial Commit

---
+++ CustomPopupsWithNavigationView/CustomPopupsWithNavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPopupsWithNavigationViewApp.swift
+//  CustomPopupsWithNavigationView
+//
+//  Created by Quang Bao on 26/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomPopupsWithNavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
